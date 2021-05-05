e3fa730ace3cdc5aa7837535306355b104f11d87
Vasil Vasilev <vasetousa@gmail.com>
2021-05-05 14:13:32 +0200

    amended

--- Ranking.py
+++ Ranking.py
@@ -2,11 +2,10 @@ END_OF_CONTESTS = "end of contests"
 END_OF_SUBMISSIONS = "end of submissions"
 contests = {}
 string = ''
-users_dict_counter = 1
-valid_users_contest_data = {'contest': "", 'name': "", 'points': 0}
+valid_users_contest_data = {}
 total_points = {}
 
-not_in_dict = False
+low_points = False
 while True:
     string = input()
     if string == END_OF_CONTESTS:
@@ -23,38 +22,35 @@ while True:
         pass_from_contests = contests.get(contest)
         if password == pass_from_contests:
 
-# check if contests is in the list, if the name is the same and if the points are more than the points in the dict
-            for ke, va in valid_users_contest_data.copy().items():
-                for contest_from_dict, value in va.copy().items():
-                    if contest_from_dict == contest:
-                        for username_from_dict, points_from_dict in value.copy().items():
-                            if username_from_dict == username:
-                                not_in_dict = True
-                                if points > points_from_dict:
-                                    valid_users_contest_data[ke] = {contest_from_dict:{username: points}}
+            # check if contests is in the list, if the name is the same and if the points are more than the points in the dict
+            if username in valid_users_contest_data:
+                for key, dict_values in valid_users_contest_data.copy().items():
+                    for key2, dict_values_2 in dict_values.copy().items():
+                        if key == username:
+                            if key2 == contest:
+                                if points > dict_values_2:
+                                    valid_users_contest_data[key].update({contest: points})
+                                else:
+                                    low_points = True
                             else:
-                                not_in_dict = False
-            users_dict_counter += 1
-            if not not_in_dict:
-                valid_users_contest_data[users_dict_counter] = {contest: {username: points}}
-                if username not in total_points:
-                    total_points[username] = points
-                else:
+                                valid_users_contest_data[username].update({contest: points})
+            else:
+                valid_users_contest_data[username] = {contest: points}
+
+            if username not in total_points:
+                total_points[username] = points
+            else:
+                if not low_points:
                     total_points[username] += points
-                not_in_dict = False
+                low_points = False
 
 sorted_numbers = sorted(total_points.items(), key=lambda kvp: -kvp[1])
-# sorted_users = sorted(valid_users_contest_data.items(), key=lambda kvp: -kvp[1])
+sorted_users = sorted(valid_users_contest_data[username].items(), key=lambda kvp: -kvp[1], kvp[0])
 
 print(f"Best candidate is {sorted_numbers[0][0]} with total {sorted_numbers[0][1]} points.")
 print("Ranking")
 
-for ke, va in valid_users_contest_data.items():
-    for contest_from_dict, value in va.items():
-        for username_from_dict, points_from_dict in value.items():
-            username = username_from_dict
-            print(username)
-            if username == username_from_dict:
-                print(contest_from_dict)
+for key, dict_values in valid_users_contest_data.items():
+    for contest_from_dict, value in dict_values.items():
+        pass
 print()
-
---
+++ test.py
@@ -0,0 +1,28 @@
+Part One Interview:success
+Js Fundamentals:Pesho
+C# Fundamentals:fundPass
+Algorithms:fun
+end of contests
+C# Fundamentals=>fundPass=>Tanya=>350
+Algorithms=>fun=>Tanya=>380
+Part One Interview=>success=>Nikola=>120
+Java Basics Exam=>pesho=>Petkan=>400
+Part One Interview=>success=>Tanya=>220
+OOP Advanced=>password123=>BaiIvan=>231
+C# Fundamentals=>fundPass=>Tanya=>250
+C# Fundamentals=>fundPass=>Nikola=>200
+Js Fundamentals=>Pesho=>Tanya=>400
+end of submissions
+
+
+
+
+
+Part One Interview:success
+Js Fundamentals:Pesho
+C# Fundamentals:fundPass
+Algorithms:fun
+end of contests
+C# Fundamentals=>fundPass=>Tanya=>250
+C# Fundamentals=>fundPass=>Tanya=>100
+end of submissions
