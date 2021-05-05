9468f8b38c14a4850be8f9933ebd2122fa746f91
Vasil Vasilev <vasetousa@gmail.com>
2021-05-05 21:14:58 +0200

    amended

--- test.py
+++ test.py
@@ -1,28 +1,6 @@
-Part One Interview:success
-Js Fundamentals:Pesho
-C# Fundamentals:fundPass
-Algorithms:fun
-end of contests
-C# Fundamentals=>fundPass=>Tanya=>350
-Algorithms=>fun=>Tanya=>380
-Part One Interview=>success=>Nikola=>120
-Java Basics Exam=>pesho=>Petkan=>400
-Part One Interview=>success=>Tanya=>220
-OOP Advanced=>password123=>BaiIvan=>231
-C# Fundamentals=>fundPass=>Tanya=>250
-C# Fundamentals=>fundPass=>Nikola=>200
-Js Fundamentals=>Pesho=>Tanya=>400
-end of submissions
+dict_1 = {"Pesho": {"WS Fundamentals": 200, "Java": 500}, "Ivan": {"C# Fundamentals": 300, "AJava": 400}}
 
+sorted_d = sorted(dict_1.items(), key=lambda kvp: kvp[0])
 
+print()
 
-
-
-Part One Interview:success
-Js Fundamentals:Pesho
-C# Fundamentals:fundPass
-Algorithms:fun
-end of contests
-C# Fundamentals=>fundPass=>Tanya=>250
-C# Fundamentals=>fundPass=>Tanya=>100
-end of submissions
