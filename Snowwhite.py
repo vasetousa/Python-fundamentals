6fafc739b4fc165e558e7b8f3ea958dcb9873cfd
Vasil Vasilev <vasetousa@gmail.com>
2021-05-07 11:11:23 +0200

    amended

--- Snowwhite.py
+++ Snowwhite.py
@@ -1,34 +1,33 @@
 data = input()
 store = {}
 name_counter = {}
-counter = 0
 is_stored = False
 
 while not data == "Once upon a time":
-    string = data.split(" <:> ")
-    dwarf = string[0]
-    hat_color = string[1]
-    power = int(string[2])
+    dwarf, hat_color, power = data.split(" <:> ")
+    power = int(power)
+
 #   Check for same name
     if dwarf not in name_counter:
         name_counter[dwarf] = 1
-        store[counter] = (dwarf, hat_color, power)
-        counter += 1
-    else:           # imame same name ->>>
-        for key, value in store.items():
-            if value[0] == dwarf and value[1] == hat_color: # ako i hat e same color
-                if value[2] < power:
-                    store[key] = (dwarf, hat_color, power)  # ako power > store the same dwarf with the new power
-                    is_stored = True
+        store[dwarf] = {hat_color: power}
+    else:           # we have same name ->>>
+        for key, value in store.copy().items():
+            for k, v in value.copy().items():
+                if k == hat_color:
+                    if v < power:
+                        store[key].update({hat_color: power})  # ako power > store the same dwarf with the new power
+                        is_stored = True
+                else:
+                    store[key].update({hat_color: power})
         if not is_stored:
             name_counter[dwarf] += 1
-            store[counter] = (dwarf, hat_color, power)
-            counter += 1
-
 
     data = input()
 
-sorted_powers1 = dict(sorted(store.items(), key=lambda kvp: (-(kvp[1][2]), kvp[1][1])))
-for key, value in sorted_powers1.items():
-    print(f'({value[1]}) {value[0]} <-> {value[2]}')
+sorted_powers = sorted(store.items(), key=lambda kvp: (kvp[0][1], kvp[1]))
+for key, value in sorted_powers:
+    for k, v in value.items():
+        print(f'({k}) {key} <-> {v}')
+
 print()
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-dict_1 = {"Pesho": {"WS Fundamentals": 200, "Java": 500}, "Ivan": {"C# Fundamentals": 300, "AJava": 400}}
-
-sorted_d = sorted(dict_1.items(), key=lambda kvp: kvp[0])
-
-print()
-
+Pesho <:> Red <:> 100
+Pesho <:> Blue <:> 200
+Pesho <:> Red <:> 100
+Gosho <:> Blue <:> 400
+Once upon a time
