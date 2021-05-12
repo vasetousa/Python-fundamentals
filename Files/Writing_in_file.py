d54e17d67cccaeec86fe61e29393f0c3663082cf
Vasil Vasilev <vasetousa@gmail.com>
2021-05-12 12:30:43 +0200

    amended name of file dir to scratchpad

--- Files/Writing_in_file.py
+++ Files/Writing_in_file.py
@@ -1,5 +1,16 @@
-cities = ["Adelaide", "San Francisco", "New York", "Darwin", "Sydney"]
+# cities = ["Adelaide", "San Francisco", "New York", "Darwin", "Sydney"]
+#
+# with open("/Users/Vasil/Desktop/Cities.txt", 'w') as city_file:
+#     for city in cities:
+#         print(city, file=city_file, flush=True) # flush the memory buffer.
+#         # not needed in modern computers
 
-with open("/Users/Vasil/Desktop/Cities.txt", 'w') as city_file:
-    for city in cities:
-        print(city, file=city_file)
+cities = []
+with open("/Users/Vasil/Desktop/Cities.txt", 'r') as city_file:
+    for city in city_file:
+        cities.append(city.strip('\n'))
+
+print(cities)
+for city in cities:
+    print(city)
+
