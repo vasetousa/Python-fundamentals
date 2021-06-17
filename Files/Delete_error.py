69681b543d5ad401d05c1a44d0076be1d124d278
Vasil <vasetousa@gmail.com>
2021-06-17 12:46:46 +0200

    Files dir moved to Advanced

--- Files/Delete_error.py
+++
@@ -1,9 +0,0 @@
-import os
-# create file
-# file = open('try_to_delete_me.txt', 'w')
-
-# delete file 2 tmes
-try:
-    os.remove('try_to_delete_me.txt')
-except:
-    print("File already deleted!")
